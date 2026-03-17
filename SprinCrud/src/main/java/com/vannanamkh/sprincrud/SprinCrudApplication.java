bcc1488bedbf557196d54f7c1af335997f43acb3
NamVanna <namvanna62@gmail.com>
2026-03-17 21:27:29 +0700

    springProject

---
+++ SprinCrud/src/main/java/com/vannanamkh/sprincrud/SprinCrudApplication.java
@@ -0,0 +1,13 @@
+package com.vannanamkh.sprincrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinCrudApplication.class, args);
+    }
+
+}
---
+++ SprinCrud/src/test/java/com/vannanamkh/sprincrud/SprinCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vannanamkh.sprincrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
